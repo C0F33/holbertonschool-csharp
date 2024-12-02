699595013ce9162c6c8fedb8141ed79a1b04e2a8
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-02 17:30:44 -0500

    99

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         int number = 972;
-        Console.WriteLine(string.Format("{0} Mission Street\n", number));
+        Console.WriteLine(string.Format("{0} Mission Street", number));
     }
 }
