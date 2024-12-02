3e025370b3f19bba45688b2c41f23a7d91752c08
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-02 18:17:03 -0500

    99

--- csharp-hello_world/4-print_integer/obj/Debug/net9.0/4-print_integer.AssemblyInfo.cs
+++ csharp-hello_world/4-print_integer/obj/Debug/net9.0/4-print_integer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad981775d65eb96cbe2926d5a2a538bfcc7b2cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760d58b9f822211c4aa84cdefcff5adfc92ab7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
