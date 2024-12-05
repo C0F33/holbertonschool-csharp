3bf2b9f48944eb702b7d7b6591170320e4f3bfa7
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-05 16:13:10 -0500

    889

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        // Get the absolute value of the last digit
+        int lastDigit = Math.Abs(number % 10);
+        
+        // Print the last digit
+        Console.Write(lastDigit);
+        
+        // Return the last digit
+        return lastDigit;
+    }
+}
