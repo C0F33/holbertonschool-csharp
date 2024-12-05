9f33e11650deea68bfcb7402d7bc620375b4229e
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-05 16:49:06 -0500

    99011

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write(new string(' ', i));
+            Console.WriteLine("\\");
+        }
+    }
+}
