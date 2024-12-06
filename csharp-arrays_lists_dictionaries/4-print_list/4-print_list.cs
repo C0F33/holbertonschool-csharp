8c00414a5738f036c4f100012ea089c7efd1caa8
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-06 16:27:18 -0500

    040

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,30 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Program
+class List
 {
-    // Method to create and print a list
     public static List<int> CreatePrint(int size)
     {
-        // Create a new list
-        List<int> newList = new List<int>();
-
-        // If size is negative, print error and return null
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
 
-        // Add elements to the list
+        List<int> newList = new List<int>();
+
         for (int i = 0; i < size; i++)
         {
             newList.Add(i);
-            Console.Write(i); // Print each number in the list
+            Console.Write(i);
             if (i != size - 1)
-                Console.Write(" "); // Add space between elements, except the last one
+                Console.Write(" ");
         }
-        // Add a dollar sign at the end of the line
-        Console.WriteLine(); // Print new line at the end
+
+        Console.WriteLine();
+
         return newList;
     }
+}
