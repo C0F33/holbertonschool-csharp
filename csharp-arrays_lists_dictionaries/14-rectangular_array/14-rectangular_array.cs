5216cdb40ffc46cecfa7b9058737c7881e8f2f7a
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-08 14:22:02 -0400

    14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] mat = new int[5, 5];
+        for (int f = 0; f < 5; f++)
+        {
+            for (int c = 0; f < 5; c++)
+                mat[f, c] = 0;
+        }
+
+        mat[2, 2] = 1;
+        for (int f = 0; f < 5; f++)
+        {
+            for (int c = 0; c < 5; c++)
+                System.Console.Write("{0}{1}", mat[f,c], c < 4 ? " ": " ");
+            Console.WriteLine();
+        }
+    }
+}
