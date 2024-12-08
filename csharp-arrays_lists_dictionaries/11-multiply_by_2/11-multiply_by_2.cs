e4fd43bd245fd21d6efc519f80d73db996e41dcc
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-08 14:01:20 -0400

    changed the mltiplication method

--- csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using Sytem.Collections.Generic;
+using System.Collections.Generic;
 
 class Dictionary
 {
-    public static Dictionary<string, int> MultipllyBy2(Dictionary<string, int> myDict)
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
     {
         Dictionary<string, int> newDict = new Dictionary<string, int>();
+
         foreach (KeyValuePair<string, int> entry in myDict)
-            newDict.Add(entry.Key, entry.Value*2);
+            newDict[item.Key] = item.Value * 2;
         return newDict;
-        
+
     }
 }
