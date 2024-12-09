fbe74de4e38411713ab42057478c59e6f1b14e3c
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-09 13:04:00 -0400

    2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> UniqueValues(List<int> List1, List<int> List2)
+    {
+        var newList = new List<int>();
+
+        foreach (int i in List1)
+        {
+            if (!List2.Contains(i) && !newList.Contains(i))
+            {
+                newList.Add(i);
+            }
+        }
+
+        foreach (int j in List2)
+        {
+            if (!List1.Contains(j) && !newList.Contains(j))
+            {
+                newList.Add(j);
+            }
+        }
+
+        newList.Sort();
+        return newList;
+    }
+}
