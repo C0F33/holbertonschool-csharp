783539b4d455bad04d09f586cd4c3b8236b3b562
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-09 13:09:54 -0400

    gave to ai to fix typos

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class List
+class List // Class name remains the same for compliance but should be unique to avoid conflicts.
 {
-    public static List<int> UniqueValues(List<int> List1, List<int> List2)
+    // Renamed the method to match the required prototype
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        var newList = new List<int>();
+        var newList = new List<int>(); // Ensures newList is properly initialized
 
-        foreach (int i in List1)
+        // Add elements from list1 that are not in list2
+        foreach (int i in list1)
         {
-            if (!List2.Contains(i) && !newList.Contains(i))
+            if (!list2.Contains(i) && !newList.Contains(i)) // Corrected `List2` to `list2`
             {
                 newList.Add(i);
             }
         }
 
-        foreach (int j in List2)
+        // Add elements from list2 that are not in list1
+        foreach (int j in list2)
         {
-            if (!List1.Contains(j) && !newList.Contains(j))
+            if (!list1.Contains(j) && !newList.Contains(j)) // Corrected `List1` to `list1`
             {
                 newList.Add(j);
             }
         }
 
+        // Sort the resulting list
         newList.Sort();
+
         return newList;
     }
 }
