fe60fdd970e48fd58537cbfd1998ebbc5a875ece
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-10 21:48:50 -0400

    8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int node = 0;
+        if(myLList.Cointains(value))
+        {
+            foreach (int i in myLList)
+            {
+                if (i == value)
+                    return node;
+                node += i;
+            }
+        }
+        else
+        {
+            return (-1)
+        }
+
+        return -1;
+    }
+}
