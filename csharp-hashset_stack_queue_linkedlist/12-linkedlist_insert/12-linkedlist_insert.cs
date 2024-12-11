7dbabfd608fecbd982412b08fc18ecc61ac021dd
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-11 08:22:31 -0400

    12

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿;using System;
 using System.Collections.Generic;
 
 class LList
@@ -6,17 +6,17 @@ class LList
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
         LinkedListNode<int> current = myLList.First;
-        while (current != null)
+        while(current != null)
         {
-            if (current.Value < n)
+            if(current.Value < n)
                 current = current.Next;
             else
             {
-                myLList.AddBefore(current, n)
+                myLList.AddBefore(current, n);
                 return myLList.First;
-            }
+            }   
         }
         myLList.AddLast(n);
-        return myLList.First;
+        return myLList.First;  
     }
 }
