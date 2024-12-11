262f660a84ce57dd3e57ed705105d96e97931f21
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-11 08:28:21 -0400

    13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int i = 0;
+        while (current != null)
+        {
+            if(i == index)
+                myLList.Remove(current);
+            x += 1;
+            current = current.Next;
+        }
+    }
+}
