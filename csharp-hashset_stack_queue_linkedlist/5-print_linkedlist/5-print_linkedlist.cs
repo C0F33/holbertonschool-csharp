a76844a5d53ede3096e4d70f8919bf9f8766ce39
Angel Bermudez <angelo1998bg@gmail.com>
2024-12-10 21:28:41 -0400

    3

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedLists<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            LinkedLists<int> listEmpty = new LinkedLists<>();
+            return listEmpty;
+        }
+
+        List<int> nList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            nList.Add(i);
+            Consle.WriteLine(i);
+        }
+
+        LinkedList<int> nList1 = new LinkedLists<int>(nList);
+        return nList1;
+    }
+}
