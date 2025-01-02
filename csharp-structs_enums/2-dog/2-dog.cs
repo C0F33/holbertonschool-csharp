371455ce9a056539709916bac7481aa6f24ba909
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-02 11:49:19 -0400

    02

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -5,18 +5,25 @@ public enum Rating
     Great,
     Excellent
 }
+
 public struct Dog
 {
     public string name;
     public float age;
     public string owner;
     public Rating rating;
-}
 
-public Dog(string name, float age, string owner, Rating rating)
-{
-    this.name = name;
-    this.age = age;
-    this.owner = owner;
-    this.rating = rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        name = name;
+        age = age;
+        owner = owner;
+        rating = _rating;
+    }
+
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
 }
