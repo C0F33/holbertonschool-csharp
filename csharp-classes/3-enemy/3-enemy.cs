b17cfd757ca381851f4f46e456ce72e249272591
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-07 18:51:40 -0400

    09

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> public class zombie</summary>
+    class Zombie
+    {
+        /// <summary> field health</summary>
+        public int health;
+        /// public constructor.
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        public int getHealth()
+        {
+            return this.health;
+        }
+    }
+}
