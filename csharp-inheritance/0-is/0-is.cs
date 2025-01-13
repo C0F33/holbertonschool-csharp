86f809667629a6bc1939dc3e88c25540f418b908
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-13 09:10:38 -0400

    00

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Obj
+{
+    ///<summary>returns True if the object is an int, otherwise return False</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
