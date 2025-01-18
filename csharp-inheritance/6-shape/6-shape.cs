0c100b3fd3a5c8e1ac6097120c3fdb2849e470ea
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:45:36 -0400

    06

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Rectangle : Shape
+{
+    ///<summary>Private fields</summary>
+    private int width;
+    private int height;
+
+    ///<summary>Public property: Width</summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            width = value;
+        }
+    }
+
+    ///<summary>Public property: Height</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            height = value;
+        }
+    }
+}
