4e076981179658d0f9c43aed797f5a1b9c5e962f
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:06:33 -0400

    02

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 class Obj
 {
     /// <summary>Checks if object inherits from derivedType</summary>
-    
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return derivedType.IsSubclassOf(baseType);
     }
+}
