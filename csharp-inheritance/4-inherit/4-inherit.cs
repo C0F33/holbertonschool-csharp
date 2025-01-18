8e1f7d6b8213cd9e50aa640c9601cbceceb16348
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:29:46 -0400

    04

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Sytem.Collections.Generic;
+/// <summary>Empty animal class</summary>
+class Animal
+{
+    
+}
+/// <summary>Dog class inherits from Animal</summary>
+class Dog : Animal
+{
+    
+}
