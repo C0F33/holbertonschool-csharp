7fd8eb7d6fa7e76fef5b4c28b2f992fe41a4fcda
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:40:53 -0400

    05

--- csharp-inheritance/5-shape/5-shape.cs
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 
-class Shape
+class Rectangle : Shape
 {
-    // Virtual method Area
-    public virtual int Area()
+    ///<summary>Private fields</summary>
+    private int width;
+    private int height;
+
+    ///<summary>Public property: Width</summary>
+    public int Width
     {
-        throw new NotImplementedException("Area() is not implemented");
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            width = value;
+        }
+    }
+
+    ///<summary>Public property: Height</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            height = value;
+        }
     }
 }
