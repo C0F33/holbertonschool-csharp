b8f80878480e622c3adeb358a9b7d326d8d36406
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 13:47:30 -0400

    01

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary> Returns true if instance is Array <summary/> ///
+>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (IsInstanceOfArray(obj.GetType()));
+    }
+}
