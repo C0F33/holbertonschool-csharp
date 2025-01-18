3f0d037879e16caf2175bfc8c22a1c9559b3f7fa
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:38:28 -0400

    05

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Shape
+{
+    // Virtual method Area
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
