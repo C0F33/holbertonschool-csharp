2efd051bc9c9b863d56a21717d31fa8c08b70f15
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-18 14:18:57 -0400

    03

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        if (myObj == null)
+        {
+            Console.WriteLine("Object is null.");
+            return;
+        }
+
+        Type type = myObj.GetType();
+
+        // Print the properties
+        Console.WriteLine("Properties:");
+        foreach (PropertyInfo property in type.GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        // Print the methods
+        Console.WriteLine("Methods:");
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
