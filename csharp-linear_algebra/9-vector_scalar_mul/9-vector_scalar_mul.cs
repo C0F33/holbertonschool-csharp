d06967ccb545c7075eeb65b638c09477924d3bd9
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-31 09:37:14 -0400

    09

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return new double[] { -1 };
+        }
+        double[] sum = new double[vector.Length];
+        for (int i =0; i < vector.Length; i++)
+        {
+            sum[i] = vector[i] * scalar;
+
+        }
+        return sum;
+    } 
+}
