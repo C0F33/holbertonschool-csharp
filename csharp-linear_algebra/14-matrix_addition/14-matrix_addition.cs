9fc606fe268707c3e7476a8acc2bc9360870cc92
Angel Bermudez <angelo1998bg@gmail.com>
2025-02-01 15:13:54 -0400

    14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,39 +1,29 @@
 ﻿using System;
 
 /// <summary>
-/// MatrixMath class
+/// MatrixMath main class.
 /// </summary>
 class MatrixMath
 {
     /// <summary>
-    /// Adds two matrices if they have the same dimensions.
+    /// Method that adds two matrices and returns the resulting matrix.
     /// </summary>
-    /// <param name="matrix1">The first matrix.</param>
-    /// <param name="matrix2">The second matrix.</param>
-    /// <returns>
-    /// A new matrix containing the sum of matrix1 and matrix2.
-    /// Returns {{-1}} if the matrices cannot be added.
-    /// </returns>
-    public static double[,] Add(double[,] matrix1, double[,] matrix2)
-    {
-        // Get dimensions of the matrices
+    /// <param name="matrix1">First matrix (2D or 3D)</param>
+    /// <param name="matrix2">Second matrix (2D or 3D)</param>
+    /// <returns>returns the resulting matrix.</returns>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2){
+        
         if (matrix1.Length != matrix2.Length ||
             matrix1.GetLength(0) != matrix1.GetLength(1) ||
             matrix2.GetLength(0) != matrix2.GetLength(1)){
             return (new Double[,]{{-1}});
-
-        // Initialize result matrix with correct dimensions
-        double[,] result = new double[row0, col0];
-
-        // Nested loops to iterate through rows and columns
-        for (int i = 0; i < row0; i++)
-        {
-            for (int j = 0; j < col0; j++)
-            {
-                result[i, j] = matrix1[i, j] + matrix2[i, j];
+        }
+        double[,] result = new double[matrix1.GetLength(0),matrix1.GetLength(1)];
+        for (int y = 0; y < matrix1.GetLength(0); y++){
+            for (int x = 0; x < matrix1.GetLength(1); x++){
+                result[y, x] = matrix1[y,x] + matrix2[y,x];
             }
         }
-        
         return result;
     }
 }
