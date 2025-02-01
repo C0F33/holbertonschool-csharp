f2b609eef17a3c8f78ed416f3664065981ee7bcd
Angel Bermudez <angelo1998bg@gmail.com>
2025-02-01 15:00:14 -0400

    14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,38 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
+
 /// <summary>
 /// MatrixMath class
 /// </summary>
 class MatrixMath
 {
     /// <summary>
-    /// matrix addition
+    /// Adds two matrices if they have the same dimensions.
     /// </summary>
-    /// <param name="matrix1"></param>
-    /// <param name="matrix2"></param>
-    /// <returns></returns>
+    /// <param name="matrix1">The first matrix.</param>
+    /// <param name="matrix2">The second matrix.</param>
+    /// <returns>
+    /// A new matrix containing the sum of matrix1 and matrix2.
+    /// Returns {{-1}} if the matrices cannot be added.
+    /// </returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-       /// first lets get our variables set up
-       int row0 = matrix1.GetLength(0);
-       int col0 = matrix1.GetLength(1);
-       int row1 = matrix2.GetLength(0);
-       int col1 = matrix2.GetLength(1);
-       ///now check for matching lengths
-       if row1 != row0 || col0 != col1
-       {
-           return new double[,] { { -1 } };
-       }
-       ///result variable set up
-       double[,] result = new Double[,] {{row0, col0} };
-       ///nested loops to iterate through values in matrix, columns and rows
-       for (int i = 0; i < row0; i++)
-       {
-           for (int j =0; j < col0; j++)
-           {
-               result[i, j] = matrix1[i, j] + matrix2[i, j];
-           }
-       }
-       return result;
+        // Get dimensions of the matrices
+        int row0 = matrix1.GetLength(0);
+        int col0 = matrix1.GetLength(1);
+        int row1 = matrix2.GetLength(0);
+        int col1 = matrix2.GetLength(1);
+
+        // Check if matrices have matching dimensions
+        if (row1 != row0 || col0 != col1)
+        {
+            return new double[,] { { -1 } }; // Return error matrix
+        }
+
+        // Initialize result matrix with correct dimensions
+        double[,] result = new double[row0, col0];
+
+        // Nested loops to iterate through rows and columns
+        for (int i = 0; i < row0; i++)
+        {
+            for (int j = 0; j < col0; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        
+        return result;
     }
 }
