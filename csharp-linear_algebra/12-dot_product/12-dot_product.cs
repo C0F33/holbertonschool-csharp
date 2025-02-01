586afec707b9b2831fcd819c763cc7a7fd091421
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-31 23:00:19 -0400

    09

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -17,8 +17,10 @@ class VectorMath
         
         if (vector1.Length != vector2.Length ||
             vector1.Length != 3 && vector1.Length != 2 || 
-            vector2.Length != 3 && vector2.Length != 2){
+            vector2.Length != 3 && vector2.Length != 2)
+        {
             return (-1);
+        }
 
         double sum = 0;
         for (int i = 0; i < vector1.Length; i++)  // Fix loop condition
