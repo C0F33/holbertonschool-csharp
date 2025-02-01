c1d1d5191bb0be9e8cd021eb835c46ab4db2ed5b
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-31 22:58:38 -0400

    1212

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -15,10 +15,10 @@ class VectorMath
     public static double DotProduct(double[] vector1, double[] vector2)
     {
         
-        if (vector1.Length != vector2.Length)
-        {
-            throw new ArgumentException("Vectors must have the same length.");
-        }
+        if (vector1.Length != vector2.Length ||
+            vector1.Length != 3 && vector1.Length != 2 || 
+            vector2.Length != 3 && vector2.Length != 2){
+            return (-1);
 
         double sum = 0;
         for (int i = 0; i < vector1.Length; i++)  // Fix loop condition
