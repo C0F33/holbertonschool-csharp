b9178b10ed3af9cc72cb2ca6ad23a2b3d65dfa62
Angel Bermudez <angelo1998bg@gmail.com>
2025-01-31 22:42:56 -0400

    12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length)
+        {
+            throw new ArgumentException("Vectors must have the same length.");
+        }
+
+        double sum = 0;
+        for (int i = 0; i < vector1.Length; i++)  // Fix loop condition
+        {
+            sum += vector1[i] * vector2[i];  // Compute dot product
+        }
+
+        return sum;  // Return result
+    }
+}
